3a26c77cb8ad0796778733c400f9c91cec4f9e64
siddhantsiddh15 <94108842+siddhantsiddh15@users.noreply.github.com>
2021-11-27 00:43:42 +0530

    Add files via upload

---
+++ app/src/main/java/com/nicheclout/YogActivity.java
@@ -0,0 +1,16 @@
+package com.nicheclout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nicheclout.exerciselist.R;
+
+public class YogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yog);
+    }
+}
